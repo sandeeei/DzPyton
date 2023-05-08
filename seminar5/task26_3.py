b4c08cda4509002094894e06a1fe8a25a8758b51
Sergei <sandeeey@example.com>
2023-05-08 12:00:56 +0300

    task26_3

--- seminar5/task26_3.py
+++ seminar5/task26_3.py
@@ -2,3 +2,11 @@
 # треугольное число (сумма чисел от 1 до N) для числа N 
 # ЧЕРЕЗ РЕКУРСИЮ и 
 # без циклов
+
+N = int(input("Введите число:  "))
+def faktor(N):
+    
+    if N==1:
+        return 1
+    return N+faktor(N-1)
+print(faktor(N))
